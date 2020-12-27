507154c40851e9d0ac17cdde9bbff3c8ca5039b1
Chirstopher Gjelten <onlygamernerd@gmail.com>
2020-12-27 16:40:10 +0100

    fixed fxmanifest paths

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,6 +1,6 @@
 fx_version "adamant"
 game "gta5"
 
-client_script "dist/*.client.js"
+client_script "dist/client/*.client.js"
 
-server_script "dist/*.server.js"
+server_script "dist/server/*.server.js"
