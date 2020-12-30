af8a26821eca7264a30fa820232932a413a656d7
Christopher <onlygamernerd@gmail.com>
2020-12-30 01:43:11 +0100

    imports

--- server.ts
+++ server.ts
@@ -1,5 +1,7 @@
 import './src/database/db';
 import './src/database/db_utils';
-import './src/core/server';
-import './src/core/functions';
+import './src/core/server/server';
+import './src/core/server/functions';
 import './src/typings/player';
+import './src/classes/server/Player';
+import './src/core/server/spawn';
