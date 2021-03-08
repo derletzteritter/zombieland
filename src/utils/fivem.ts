44d2767da6ae27114eb4ac6857a5d604ec50a63c
Christopher Gjelten <onlygamernerd@gmail.com>
2021-03-08 22:24:34 +0100

    Update fivem.ts

--- src/utils/fivem.ts
+++ src/utils/fivem.ts
@@ -1,6 +1,6 @@
 export const Delay = (ms: number) => new Promise((res) => setTimeout(res, ms))
 
-function getDistance(player: number[], target: number[]) {
+export function getDistance(player: number[], target: number[]) {
   return (
     player
       .map((x, i) => Math.abs(x - target[i]) ** 2) // square the difference
